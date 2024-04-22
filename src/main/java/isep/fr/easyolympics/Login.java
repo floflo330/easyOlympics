d98daa054848b231c55df94d1bf91bef73175369
floflo330 <florent.lmdb@icloud.com>
2024-04-22 11:25:45 +0200

    Modifier CSS

--- src/main/java/isep/fr/easyolympics/Login.java
+++ src/main/java/isep/fr/easyolympics/Login.java
@@ -4,25 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Login extends Application {
 
+    public Text labelNomPage;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Charger le fichier FXML
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
 
-        // Créer une nouvelle scène
         Scene scene = new Scene(root);
 
-        // Mettre la scène dans la fenêtre principale (primaryStage)
         primaryStage.setScene(scene);
 
-        // Définir le titre de la fenêtre
         primaryStage.setTitle("EasyOlympics - Connexion");
 
-        // Afficher la fenêtre
         primaryStage.show();
     }
 
