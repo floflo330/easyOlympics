08f7c8426b6a6b0b8c8f4fda6e43eaa8c1d51bf6
floflo330 <florent.lmdb@icloud.com>
2024-04-22 14:51:18 +0200

    Ajout Home

---
+++ src/main/java/isep/fr/easyolympics/Home.java
@@ -0,0 +1,33 @@
+package isep.fr.easyolympics;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Home extends Application {
+
+    public Text labelNomPage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.setTitle("EasyOlympics - Accueil");
+
+        primaryStage.getIcons().add(new Image(Login.class.getResourceAsStream("img/logojo.png")));
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
