501dde900c3680bece92aff815d43e07fa950296
floflo330 <florent.lmdb@icloud.com>
2024-04-23 15:10:46 +0200

    Modif

--- src/main/java/isep/fr/easyolympics/model/TestDatabaseConnection.java
+++ src/main/java/isep/fr/easyolympics/model/TestDatabaseConnection.java
@@ -13,33 +13,24 @@ public class TestDatabaseConnection {
         ResultSet resultSet = null;
 
         try {
-            // Obtention de la connexion à la base de données
             connection = Database.getConnection();
 
-            // Requête SQL pour sélectionner toutes les lignes de la table 'sports'
             String query = "SELECT * FROM users";
 
-            // Préparation de la déclaration
             statement = connection.prepareStatement(query);
 
-            // Exécution de la requête
             resultSet = statement.executeQuery();
-
-            // Affichage des résultats
+            System.out.println("Utilisateurs de la plateforme");
             while (resultSet.next()) {
-                // Récupération des données de chaque colonne
                 int id = resultSet.getInt("userId");
                 String userName = resultSet.getString("name");
                 String userSurname = resultSet.getString("surname");
                 String discipline = resultSet.getString("email");
-
-                // Affichage des données récupérées
                 System.out.println("ID: " + id + ", Prénom: " + userName + ", Nom: " + userSurname + ", Email: " + discipline);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            // Fermeture des ressources
             try {
                 if (resultSet != null) {
                     resultSet.close();
