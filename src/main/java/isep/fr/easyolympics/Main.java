c6855298d088a6b8ab7dbacb53dc1dc57a38559a
floflo330 <florent.lmdb@icloud.com>
2024-05-27 18:08:44 +0200

    Modif

--- src/main/java/isep/fr/easyolympics/Main.java
+++ src/main/java/isep/fr/easyolympics/Main.java
@@ -22,7 +22,6 @@ public class Main extends Application {
         //showAdminSports();
         //showAdminCountries();
         //showAdminHome();
-
     }
 
     public static void showLoginScene() throws Exception {
