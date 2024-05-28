b23f3296e75889f622a834e7086349d9b075a311
floflo330 <florent.lmdb@icloud.com>
2024-05-28 11:34:22 +0200

    Modif

--- src/main/java/isep/fr/easyolympics/adminHome.java
+++ src/main/java/isep/fr/easyolympics/adminHome.java
@@ -22,6 +22,73 @@ public class adminHome implements Initializable {
     private Button menuButton;
     @FXML
     private ContextMenu contextMenu;
+    @FXML
+    private Button athletesButton;
+    @FXML
+    private Button eventButton;
+    @FXML
+    private Button sportsButton;
+    @FXML
+    private Button countriesButton;
+    @FXML
+    private Button resultsButton;
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private void handleAthletesButton(){
+        try {
+            Main.showAdminUserList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handleEventButton(){
+        try {
+            Main.showAdminEvents();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handleSportsButton(){
+        try {
+            Main.showAdminSports();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handleCountriesButton(){
+        try {
+            Main.showAdminCountries();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handleResultsButton(){
+        try {
+            Main.showAdminResults();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handleExitButton(){
+        try {
+            Main.showHomeScene();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
